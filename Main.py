198388241a7590d5d44fd6ed7a5ac9fc728892c4
Taska222008 <thienphuoc222008@gmail.com>
2023-02-25 14:30:27 +0700

    commit

--- Main.py
+++ Main.py
@@ -1,2 +1,33 @@
 from pygame import *
-print("hello")
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Paddle_1(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        
+        if (keys[K_w]) and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if (keys[K_s]) and self.rect.y < 420:
+            self.rect.y += self.speed
+
+class Paddle_2(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        
+        if (keys[K_o]) and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if (keys[K_l]) and self.rect.y < 420:
+            self.rect.y += self.speed
+
